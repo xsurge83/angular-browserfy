8dfb6fbe79b4e576a239cb1203dea48703a0e2e1
xsurge83 <xsurge83@gmail.com>
2014-11-30 12:58:53 -0800

    updated highlight search directive

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 #### Experimenting building components with Angular and browserfy 
 
+### TODO
+1. replace sass with less
+2. change folder structure 
+3. add ui harness  
+
 
     
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,6 @@
 require('angular'); // That's right! We can just require angular as if we were in node
 
 angular.module('myApp', [
-	require('./controllers/welcomeCtrl').name, 
-	require('./directives/highlight-search').name]);
+	require('./controllers/demoCtrl').name, 
+	require('./directives/highlightSearch').name]);
  
